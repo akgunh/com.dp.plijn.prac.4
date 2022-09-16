ead8cd7314793a124bc0bfad1401ef9d84b21c07
akgunh <huseyin.akgun@student.hu.nl>
2022-09-16 17:57:10 +0200

    Initial commit

---
+++ src/AdresDAO.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+//adres interface overgenomen vanuit het erd
+public interface AdresDAO {
+    boolean save(Adres adres);
+    boolean update(Adres adres);
+    boolean delete(Adres adres);
+    Adres findByReiziger(Reiziger reiziger);
+    List<Adres> findAll();
+}
